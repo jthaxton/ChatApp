91dfd580493948451ff657b8a0f620601a4a4e57
14jthaxton <14jthaxton@gmail.com>
2020-02-06 22:20:07 -0800

    add prebuild

--- app.js
+++ app.js
@@ -7,17 +7,7 @@ const path = require('path');
 // require("./config/passport")(passport);
 const http = require('http');
 
-const WebSocketServer = require('ws').Server,
-  wss = new WebSocketServer({port: 40510}) 
-  const connections = [];
-  wss.on('connection', function (ws) {
-    connections.push(ws)
-  ws.on('message', function (message) {
-    console.log('received: %s', message)
-    connections.forEach(connection => connection.send((message)))
-  })  
 
-})
 
 const bodyParser = require("body-parser");
 
@@ -49,3 +39,16 @@ app.use(bodyParser.json());
 //   .catch(err => console.log(err));
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
+
+
+const WebSocketServer = require('ws').Server,
+  wss = new WebSocketServer({port: 40510}) 
+  const connections = [];
+  wss.on('connection', function (ws) {
+    connections.push(ws)
+  ws.on('message', function (message) {
+    console.log('received: %s', message)
+    connections.forEach(connection => connection.send((message)))
+  })  
+
+})
